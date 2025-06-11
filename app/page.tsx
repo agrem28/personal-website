ac585ad729fb315e9862f50b48bc3270a2419c0f
Andrew Gremillion <agremillion15@yahoo.com>
2025-06-10 20:15:22 -0500

    added decorative code snippet

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,57 @@ export default function Page() {
         </p>
       </div>
       <div className="py-4">
-        <div className="md:w-1/2 h-10 border rounded-t-2xl border-border">
-          <div className="flex relative top-2.5 left-2.5 text-border">
+        <div className="md:w-1/2 h-10 border rounded-t-2xl border-border bg-[#181818]">
+          <div className="flex p-2.5 text-border">
             <GoDotFill />
             <GoDotFill />
             <GoDotFill />
           </div>
         </div>
-        <div className="md:w-1/2 h-10 border rounded-b-2xl border-border relative -top-[1px]"></div>
+        <div className="md:w-1/2 p-2.5 border rounded-b-2xl border-border bg-[#1f1f1f] relative -top-[1px] font-mono whitespace-pre-wrap">
+          <p>
+            <span className="text-[#689ad3]">function </span>
+            <span className="text-[#dbdcae]">greet</span>
+            <span className="text-[#ead933]">{`(`}</span>
+            <span className="text-[#a9dafc]">name</span>
+            <span className="text-[#d4d4d4]">: </span>
+            <span className="text-[#6ec6b1]">string</span>
+            <span className="text-[#ead933]">{`)`}</span>
+            <span className="text-[#ead933]">{` {`}</span>
+          </p>
+          <p>
+            <span className="whitespace-pre-wrap">{"\t"}</span>
+            <span className="text-[#b4899f]">return </span>
+            <span className="text-[#c6947b]">{`"Hello "`}</span>
+            <span className="text-[#d4d4d4]"> + </span>
+            <span className="text-[#a9dafc]">name</span>
+            <span className="text-[#d4d4d4]">;</span>
+          </p>
+          <p>
+            <span className="text-[#ead933]">{`}`}</span>
+          </p>
+          <p className="whitespace-pre-line">{`\n`}</p>
+          <p>
+            <span className="text-[#689ad3]">const </span>
+            <span className="text-[#6fbefb]">message </span>
+            <span className="text-[#d4d4d4]"> = </span>
+            <span className="text-[#dbdcae]">greet</span>
+            <span className="text-[#ead933]">{`(`}</span>
+            <span className="text-[#c6947b]">{`"Andrew"`}</span>
+            <span className="text-[#ead933]">{`)`}</span>
+            <span className="text-[#d4d4d4]">;</span>
+          </p>
+          <p className="whitespace-pre-line">{`\n`}</p>
+          <p>
+            <span className="text-[#a9dafc]">console</span>
+            <span className="text-[#d4d4d4]">.</span>
+            <span className="text-[#dbdcae]">log</span>
+            <span className="text-[#ead933]">{`(`}</span>
+            <span className="text-[#6fbefb]">message</span>
+            <span className="text-[#ead933]">{`)`}</span>
+            <span className="text-[#d4d4d4]">;</span>
+          </p>
+        </div>
       </div>
       <div>
         <h2>My Projects</h2>
@@ -29,3 +72,11 @@ export default function Page() {
     </main>
   );
 }
+
+function greet(name: string) {
+  return "Hello " + name;
+}
+
+const message = greet("Andrew");
+
+console.log(message);
